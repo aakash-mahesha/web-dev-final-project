101b6db1682190ca8567929cd879f926e889a02c
Samanvya Tripathi <samanu09@gmail.com>
2023-08-11 16:19:31 -0400

    merge all branches

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
-import { HashRouter, Routes } from 'react-router-dom';
-import MapVerse from './mapverse';
+import { HashRouter, Routes, Navigate } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import EventForm from './mapverse/component/create_event';
+import EventForm from './components/create-event';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-
-const store = configureStore({reducer: {}});
-
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { HashRouter, Navigate } from "react-router-dom";
-import { Routes, Route } from "react-router";
-import EventPage from './components/event-page';
+import EventPage from './components/event-page/index.js';
 import SearchPage from './components/search-page';
 import Dashboard from './components/dashboard';
 import EventTable from './components/dashboard/table';
-
+import eventFormReducer from "./reducers/event-form-reducer"
 
 function App() {
+  const store = configureStore({
+    reducer: {
+        eventFormState: eventFormReducer
+    }
+})
   return (
 
     // <div className='App'>
@@ -31,19 +28,21 @@ function App() {
 
     //new:
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <HashRouter>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Navigate to="/search" />} />
-            <Route path="/search/*" element={<SearchPage />} />
-            <Route path="/dashboard" element={<Dashboard/>}/>
-            <Route path="/table" element={<EventTable />} />
-            <Route path="/create-event" element={<MapVerse/>} />
-            {/* <Route path="/search/*" element={<MapPage Component={SearchBox} />} /> */}
-            <Route path="/details/*" element={<EventPage />} />
-          </Routes>
-        </div>
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Navigate to="/search" />} />
+              <Route path="/search/*" element={<SearchPage />} />
+              <Route path="/dashboard" element={<Dashboard/>}/>
+              <Route path="/table" element={<EventTable />} />
+              <Route path="/create-event" element={<EventForm/>} />
+              {/* <Route path="/search/*" element={<MapPage Component={SearchBox} />} /> */}
+              <Route path="/details/*" element={<EventPage />} />
+            </Routes>
+          </div>
+        </HashRouter>
+      </Provider>
     </LocalizationProvider>
   );
 }
