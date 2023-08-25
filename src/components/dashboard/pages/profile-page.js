85e9fec9d203ab3bfa6724675bce145995870672
petitblue <35588651+petitblue@users.noreply.github.com>
2023-08-24 17:15:34 -0700

    profile

--- src/components/dashboard/pages/profile-page.js
+++ src/components/dashboard/pages/profile-page.js
@@ -1,9 +1,7 @@
 import { Typography, Box, Container,Paper,Grid} from "@mui/material";
 import MainLayout from "../layout/main-layout";
-import Header from "../header";
-
-import ProfileContent from "../../../form/edit-profile/profile";
 import CheckboxesGroup from "../checkbox";
+import ProfileForm from "../../../form/edit-profile/edit-profile";
 const ProfileScreen = () => {
     return(
         <>
@@ -13,14 +11,12 @@ const ProfileScreen = () => {
 
         </Box>
         <Container md={6} lg={10} xl={12}>
-            <Paper>
-                <Grid justify = "center">
-            <Header subtitle ="Profile"/>
-            </Grid>
-            </Paper>
+            
+        
+            
         <Box>
             
-        <ProfileContent/>
+        <ProfileForm/>
         <CheckboxesGroup/>
 
         </Box>
--- src/form/edit-profile/edit-profile.js
+++ src/form/edit-profile/edit-profile.js
@@ -1,94 +1,153 @@
 import React, { useEffect, useState } from 'react';
-import { TextField, Button, Grid} from '@mui/material';
+import { TextField, Button, Grid, Typography } from '@mui/material';
+import InterestTabBox from "../../components/register-component/interest-tab-box";
+
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import submitProfileFormThunk from '../../thunks/profile-form-thunks';
-function ProfileForm(){
+import { useNavigate } from "react-router";
+import { profileThunk, updateUserThunk, logoutThunk } from "../../thunks/auth-thunks";
+import authReducer from "../../reducers/auth-reducer";
+import { current } from '@reduxjs/toolkit';
+function ProfileForm() {
+    const { currentUser } = useSelector((state) => state.auth);
+    const [profile, setProfile] = useState(currentUser.details);
+    const [userUpdated, setUserUpdated] = useState(false);
+    console.log(currentUser.details)
     const dispatch = useDispatch();
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
+    const navigate = useNavigate();
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [username, setUsername] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
     const [password, setPassword] = useState('');
-    const [confirmPassword,setConfirmPassword] = useState('');
-    const [address, setAddress] = useState('');
+
+    const [email, setEmail] = useState('');
+    const [addressLine1, setAddressLine1] = useState('');
+
+    const [addressLine2, setAddressLine2] = useState('');
     const [city, setCity] = useState('');
-    const [zipCode, setZipCode] = useState('');
-    const [state, setState] = useState('');
+    const [stateName, setStateName] = useState('');
+    const [zipcode, setZipcode] = useState('');
     const [country, setCountry] = useState('');
-    const [shouldSubmit, setShouldSubmit] = useState(false);
-    // const user = useSelector((state) => state.global.user);
-    // useEffect(() => {
-    //     if (user) {
-    //     setFirstName(user.firstName);
-    //     setLastName(user.lastName);
-    //     setPhoneNumber(user.phoneNumber);
-    //     setEmail(user.email);
-    //     setAddress(user.address);
-    //     setCity(user.city);
-    //     setZipCode(user.zipCode);
-    //     setState(user.state);
-    //     setCountry(user.country);
-    //     }
-    // }, [user]);
+
+
+    const handleAddressLine1Change = (event) => {
+        const newAddressLine1 = event.target.value;
+
+        setAddressLine1(newAddressLine1);
+    };
+
     const handleFirstNameChange = (event) => {
-        setFirstName(event.target.value);
+        const newFirstname = event.target.value;
+        setFirstname(newFirstname);
     };
+
     const handleLastNameChange = (event) => {
-        setLastName(event.target.value);
+        const newLastname = event.target.value;
+        setLastname(newLastname);
     };
-    // const handlePhoneNumberChange = (event) => {
-    //     setPhoneNumber(event.target.value);
-    // };
+    const handleUsernameChange = (event) => {
+        const newUsername = event.target.value;
+        setUsername(newUsername);
+    };
     const handleEmailChange = (event) => {
-        setEmail(event.target.value);
+        const newEmail = event.target.value;
+        setEmail(newEmail);
     };
     const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
+        const newPassword = event.target.value;
+        setPassword(newPassword);
     };
-    const handleConfirmPasswordChange = (event) => {
-        setConfirmPassword(event.target.value);
-    };
-    const handleAddressChange = (event) => {
-        setAddress(event.target.value);
+    const handleAddressLine2Change = (event) => {
+        const newAddressLine2 = event.target.value;
+        setAddressLine2(newAddressLine2);
     };
     const handleCityChange = (event) => {
-        setCity(event.target.value);
-    };
-    const handleZipCodeChange = (event) => {
-        setZipCode(event.target.value);
+        const newCity = event.target.value;
+        setCity(newCity);
     };
     const handleStateChange = (event) => {
-        setState(event.target.value);
+        const newStateName = event.target.value;
+        setStateName(newStateName);
+    };
+    const handleZipCodeChange = (event) => {
+        const newZipcode = event.target.value;
+        setZipcode(newZipcode);
     };
     const handleCountryChange = (event) => {
-        setCountry(event.target.value);
-    };
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setShouldSubmit(true);
-    };
-    useEffect(() => {
-        if (shouldSubmit) {
-        dispatch(
-            submitProfileFormThunk({
-            firstName,
-            lastName,
-            email,
+        const newCountry = event.target.value;
+        setCountry(newCountry);
+    }
+
+
+
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     setShouldSubmit(true);
+    // };
+    const handleTagChange = (event, newTag) => {
+        setSelectedTags(newTag)
+    }
+    const save = async () => {
+        const updatedProfile = {
+            // _id: currentUser.details._id,
+            username,
+            firstname,
+            lastname,
             password,
-            confirmPassword,
-            address,
-            city,
-            zipCode,
-            state,
-            country,
-            })                  
-        );
-        setShouldSubmit(false); 
+            email,
+            address: {
+                addressLine1,
+                addressLine2,
+                city,
+                stateName,
+                zipcode,
+                country,
+            },
+            tags: selectedTags,
+        };
+        const dummyUser = {
+           ...currentUser.details, updatedProfile
         }
-    }, [shouldSubmit])
+
+        await dispatch(updateUserThunk(dummyUser));
+
+   
+        const response = await dispatch(profileThunk());
+        if(response){
+            setUserUpdated(true);
+        }
+        
+    };
+
+    useEffect(() => {
+        const loadProfile = async () => {
+
+            try {
+                const { payload } = await dispatch(profileThunk());
+                setProfile(payload);
+
+            } catch (error) {
+                console.error(error);
+                navigate("/login");
+            }
+        };
+
+        loadProfile();
+    }, []);
+    console.log(profile)
+
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <Typography component="h4" variant="h4" sx={{ textAlign: 'left', mb: 2 }}>
+                Welcome {currentUser.details.firstname} {currentUser.details.lastname}!
+            </Typography>
+            <form >
+                <Typography component="h5" variant="h5" sx={{ mb: 2 }} color="primary">
+                    Profile Information
+                </Typography>
+
                 <Grid container spacing={2}>
                     <Grid item xs={12} sm={6}>
                         <TextField
@@ -97,7 +156,7 @@ function ProfileForm(){
                             fullWidth
                             autoFocus
                             required
-                            value={firstName}
+                            value={currentUser.details.firstname}
                             onChange={handleFirstNameChange}
                         />
                     </Grid>
@@ -108,125 +167,156 @@ function ProfileForm(){
                             fullWidth
                             autoFocus
                             required
-                            value={lastName}
+                            value={currentUser.details.lastname}
                             onChange={handleLastNameChange}
                         />
                     </Grid>
-                    <Grid item xs={12} sm={6}>
+                    {/* <Grid item xs={12} sm={6}>
                         <TextField
-                            label = "Email"
-                            variant = "outlined"
+                            label="Username"
+                            variant="outlined"
                             fullWidth
                             autoFocus
                             required
-                            value = {email}
-                            onChange = {handleEmailChange}
-                        />  
+                            value={username}
+                            onChange={handleUsernameChange}
+                        />
+
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Email"
+                            variant="outlined"
+                            fullWidth
+                            autoFocus
+                            required
+                            value={email}
+                            onChange={handleEmailChange}
+                        />
 
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
-                            label = "Password"
-                            variant = "outlined"
+                            label="Password"
+                            type="password"
+                            variant="outlined"
                             fullWidth
                             autoFocus
                             required
-                            value = {password}
-                            onChange = {handlePasswordChange}
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Address"
+                            variant="outlined"
+                            fullWidth
+                            autoFocus
+                            required
+                            value={addressLine1}
+                            onChange={handleAddressLine1Change}
+                        />
+
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Address"
+                            variant="outlined"
+                            fullWidth
+                            autoFocus
+                            required
+                            value={addressLine2}
+                            onChange={handleAddressLine2Change}
+                        />
+
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="City"
+                            variant="outlined"
+                            fullWidth
+                            autoFocus
+                            required
+                            value={city}
+                            onChange={handleCityChange}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
-                            label = "Confirm Password"
-                            variant = "outlined"
+                            label="Zip Code"
+                            variant="outlined"
                             fullWidth
                             autoFocus
                             required
-                            value = {confirmPassword}
-                            onChange = {handleConfirmPasswordChange}
+                            value={zipcode}
+                            onChange={handleZipCodeChange}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
-                            label = "Address"
-                            variant = "outlined"
+                            label="State"
+                            variant="outlined"
                             fullWidth
                             autoFocus
                             required
-                            value = {address}
-                            onChange = {handleAddressChange}
-                        />  
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label = "City"
-                            variant = "outlined"
-                            fullWidth
-                            autoFocus
-                            required
-                            value = {city}
-                            onChange = {handleCityChange}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label = "Zip Code"
-                            variant = "outlined"
-                            fullWidth
-                            autoFocus
-                            required
-                            value = {zipCode}
-                            onChange = {handleZipCodeChange}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label = "State"
-                            variant = "outlined"
-                            fullWidth
-                            autoFocus
-                            required
-                            value = {state}
-                            onChange = {handleStateChange}
+                            value={stateName}
+                            onChange={handleStateChange}
 
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
-                            label = "Country"
-                            variant = "outlined"
+                            label="Country"
+                            variant="outlined"
                             fullWidth
                             autoFocus
                             required
-                            value = {country}
-                            onChange = {handleCountryChange}
+                            value={country}
+                            onChange={handleCountryChange}
                         />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Button 
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                            onChange={handleSubmit}
-                        >
-                            Submit
-                        </Button>
-                    </Grid>
+                    </Grid> */}
+
                 </Grid>
+                {/* <Grid item xs={12} sm={12} md={6}>
+
+                    <Typography component="h1" variant="h4" sx={{ mb: 2 }}>
+                        Select the tags that interest you!
+                    </Typography>
+                    <Grid container>
+                        <Grid item sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <InterestTabBox interestTabsList={profile.tags} selectedTags={selectedTags} handleTagChange={handleTagChange} />
+                        </Grid>
+                    </Grid>
+                </Grid> */}
+                <Grid item xs={12} sm={6}>
+                    <Button
+                        variant="contained"
+                        onClick={save}
+                    >
+                        Save
+
+                    </Button>
+                </Grid>
+
+
             </form>
         </div>
-
-                            
-                        
-
-
-
-
-
-
-
-    );        
             
-    
+
+
+
+
+
+
+
+
+
+
+    );
 }
+
+
 export default ProfileForm;
